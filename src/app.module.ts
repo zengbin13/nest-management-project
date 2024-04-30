8c9aeff656515880acd92047520d1e8b013a0a47
zengbin13 <1287538055@qq.com>
2024-04-30 15:06:41 +0800

    feat: 连接mysql数据库

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,23 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql', // 数据库类型
+      host: 'localhost', // 数据库主机
+      port: 3306, // 数据库端口
+      username: 'root', // 数据库用户名
+      password: '123456', // 数据库密码
+      database: 'nest_admin', // 数据库名
+      // entities: [__dirname + '/**/*.entity{.ts,.js}'], // 实体文件的路径
+      autoLoadEntities: true, //自动加载实体
+      synchronize: true, // 是否在每次应用启动时自动创建数据库表结构
+    }),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,1 +1,18 @@
-export class User {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('user')
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: number;
+
+  @Column({
+    length: 30,
+    comment: '用户名称',
+  })
+  username: string;
+
+  @Column({
+    comment: '用户密码',
+  })
+  password: string;
+}
